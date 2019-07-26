c208c516b5c07962cc0617fd5b2846c2939f6391
RonKbS <stephen.bamwite@gmail.com>
2019-07-26 20:28:20 +0300

    add settings to allow hosting on heroku

---
+++ Procfile
@@ -0,0 +1,2 @@
+release:  python e_l_p/manage.py migrate 
+web: cd e_l_p && gunicorn e_l_p.config.wsgi
--- e_l_p/e_l_p/config/default.py
+++ e_l_p/e_l_p/config/default.py
@@ -30,7 +30,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = config("HOST_IP", cast=Csv())
+ALLOWED_HOSTS = ('localhost', '127.0.0.1', 'el-platform.herokuapp.com',)
 
 
 # Application definition
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
